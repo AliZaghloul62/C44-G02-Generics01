cbd68ffe597af6737fed52f5e27b702e1704e227
alizaghloul <ali.zaghloul@nweave.com>
2025-07-25 19:11:09 +0300

    Add project files.

---
+++ C#ADV01/Program.cs
@@ -0,0 +1,34 @@
+﻿using CS_ADV01;
+
+namespace C_ADV01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region create a generic Range<T> class that represents a range of values from a minimum value to a maximum value. The range should support basic operations such as checking if a value is within the range and determining the length of the range.
+                Range<int> intRange = new Range<int>(-100, 100);
+                Console.WriteLine($"Range: {intRange}");
+                Console.WriteLine($"Is 15 in range? {intRange.IsInRange(15)}");
+                Console.WriteLine($"Is 250 in range? {intRange.IsInRange(250)}");
+                Console.WriteLine($"Length: {intRange.Length()}"); 
+
+                Console.WriteLine("=============================================");
+
+                Range<double> doubleRange = new Range<double>(-2.5, 5.75);
+                Console.WriteLine($"Range: {doubleRange}");
+                Console.WriteLine($"Is 3.14 in range? {doubleRange.IsInRange(3.14)}");
+                Console.WriteLine($"Is -3.14 in range? {doubleRange.IsInRange(-3.14)}");
+                Console.WriteLine($"Length: {doubleRange.Length()}");
+
+                Console.WriteLine("=============================================");
+
+                Range<char> charRange = new Range<char>('a', 'z');
+                Console.WriteLine($"Range: {charRange}");
+                Console.WriteLine($"Is c in range? {charRange.IsInRange('c')}");
+                Console.WriteLine($"Is C in range? {charRange.IsInRange('C')}");
+                Console.WriteLine($"Length: {charRange.Length()}");
+            #endregion
+        }
+    }
+}
---
+++ C#ADV01/Range.cs
@@ -0,0 +1,37 @@
+﻿namespace CS_ADV01
+{
+    public class Range <T> where T: IComparable<T>
+    {
+        private readonly T MinValue;
+        private readonly T MaxValue;
+        public Range(T minValue, T maxValue )
+        {
+            if(minValue.CompareTo(maxValue) > 0)
+            {
+                T temp = minValue;
+                minValue = maxValue;
+                maxValue = temp;
+            }
+
+            MinValue = minValue;
+            MaxValue = maxValue;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(MinValue) >= 0 && value.CompareTo(MaxValue) <= 0;
+        }
+
+        public dynamic Length()
+        {
+            dynamic max = MaxValue;
+            dynamic min = MinValue;
+            return max - min;
+        }
+
+        public override string ToString()
+        {
+            return $"MinValue = {MinValue} => MaxValue = {MaxValue}";
+        }
+    }
+}
